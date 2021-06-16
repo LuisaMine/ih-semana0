c542d4c05311c41b9e70c69460cf90395c6e2f44
Luisa Miné <LuisaMine@MacBook-Pro-de-USUARIO1.local>
2021-06-16 12:27:02 -0500

    base

---
+++ lunes.js
@@ -0,0 +1,36 @@
+console.log("Hola mundo")
+
+const bonafont = "Agua"
+console.log(bonafont)
+
+const hora = 1900
+
+if(hora > 0 && hora < 1159){
+  console.log("Buenos dias")
+} else if (hora >= 1200 && hora < 1759){
+  console.log("Buenas tardes")
+} else if (hora >= 1800 && hora < 2359){
+  console.log("Buenas noches")
+} else {
+  console.log("Hora no valida")
+}
+
+
+/*
+
+const 
+
+if (0000 - 1159) {
+  console.log("Buenos días")
+}
+ else if (1200 - 1759) {
+  console.log("Buenos días")
+}
+else if (1800 - 2359) {
+  console.log("Buenos días")
+else () {
+  console.log("Hora no valida")
+  }
+}
+
+*/
